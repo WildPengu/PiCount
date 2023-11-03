a714561922f2e75e7d575cefb2591eed1713f7d6
WildPengu <7241679@wp.pl>
2023-11-03 13:37:45 +0100

    Added present date as default value for SelectDate component

--- frontend/src/app/components/datePicker/SelectDate.tsx
+++ frontend/src/app/components/datePicker/SelectDate.tsx
@@ -1,12 +1,29 @@
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 
 export interface SelectDateProps {
     setDate?: (date: string) => void;
 }
 
 export const SelectDate = ({ setDate }: SelectDateProps) => {
+
+    const [day, setDay] = useState('');
+
+    useEffect(() => {
+        const formattedDate = dayjs(new Date()).format('YYYY-MM-DD');
+        setDay(formattedDate);
+    }, []);
+
+    const handleChange = (newDate: any) => {
+        const formattedDate = dayjs(newDate).format('YYYY-MM-DD');
+        setDay(formattedDate);
+    };
+
+    useEffect(() => {
+        setDate && setDate(day);
+    }, [day]);
 
     return (
         <div>
@@ -15,12 +32,8 @@ export const SelectDate = ({ setDate }: SelectDateProps) => {
                     label="Select Date"
                     minDate={dayjs('2023-01-01')}
                     maxDate={dayjs(Date())}
-                    onChange={(newDate) => {
-                        if (setDate) {
-                            const formattedDate = dayjs(newDate).format('YYYY-MM-DD');
-                            setDate(formattedDate);
-                        }
-                    }}
+                    onChange={(newDate) => handleChange(newDate)}
+                    value={dayjs(day)}
                     />
                 </LocalizationProvider>
         </div>
