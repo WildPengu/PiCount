c60af8b5c2646ea706d8632d3b9c59968fdc4817
YarynaHuk <jasmin.huk@gmail.com>
2023-12-18 13:42:26 +0100

    added BarChartComponent

--- frontend/src/app/layout/expenseChart/ExpenseChart.tsx
+++ frontend/src/app/layout/expenseChart/ExpenseChart.tsx
@@ -1,23 +1,30 @@
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import Button from '../../components/button/Button';
 import styles from './ExpenseChart.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartBar, faChartColumn, faChartPie, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { faChartColumn, faChartPie, faFilter } from '@fortawesome/free-solid-svg-icons';
 import { Modal } from '../../components/modal/Modal';
 import { FilterPanel } from '../../components/filterPanel/FilterPanel';
 import { TopPanel } from '../../components/topPanel/TopPanel';
 import { appSettings } from '../../config';
-import { PieChartComponent } from '../../components/charts/pieChart/PieChart';
+import { PieChartComponent } from '../../components/charts/pieChart/PieChartComponent';
 import { Loader } from '../../components/loader/Loader';
 import { DataItems } from '../../../types/Chart';
 import { ChartItem } from '../../components/charts/chartItem/ChartItem';
+import { BarChartComponent } from '../../components/charts/barChart/BarChartComponent';
 
 
 export const ExpenseChart = () => {
 
-    const [isModalSortedVisible, setIsModalSortedVisible] = useState(false);
     const [loading, setLoading] = useState(true);
     const [dataChart, setDataChart] = useState<DataItems>({ categories: [] });
+
+    const [selectedChart, setSelectedChart] = useState('pie');
+    const [isModalSortedVisible, setIsModalSortedVisible] = useState(false);
+  
+    const handleChartButtonClick = (chartType: SetStateAction<string>) => {
+      setSelectedChart(chartType);
+    };
     
     useEffect(() => {
         fetch(`${appSettings.apiHost}:${appSettings.apiPort}/expenses/diagrams/${appSettings.user_id}`)
@@ -36,52 +43,49 @@ export const ExpenseChart = () => {
                 setLoading(false);
             });
     }, []);
-
+    
     return (
         <div className={styles.ExpenseChart}>
-            <TopPanel headerText="My Chart">
-                <div className={styles.TopPanelContainer}>
-                    <Button>
-                        <FontAwesomeIcon icon={faChartPie} />
-                    </Button>
-                    <Button>
-                        <FontAwesomeIcon icon={faChartColumn} />
-                    </Button>
-                    <Button>
-                        <FontAwesomeIcon icon={faChartBar} />
-                    </Button>
-                    <Button 
-                        onClick={() => setIsModalSortedVisible(true)}
-                    >
-                        <FontAwesomeIcon icon={faFilter} />
-                    </Button>
-                    {isModalSortedVisible && <Modal>
-                        <FilterPanel setIsModalVisible={setIsModalSortedVisible} />
-                    </Modal>}
+          <TopPanel headerText="My Chart">
+            <div className={styles.TopPanelContainer}>
+              <Button onClick={() => handleChartButtonClick('pie')}>
+                <FontAwesomeIcon icon={faChartPie} />
+              </Button>
+              <Button onClick={() => handleChartButtonClick('column')}>
+                <FontAwesomeIcon icon={faChartColumn} />
+              </Button>
+              <Button onClick={() => setIsModalSortedVisible(true)}>
+                <FontAwesomeIcon icon={faFilter} />
+              </Button>
+              {isModalSortedVisible && (
+                <Modal>
+                  <FilterPanel setIsModalVisible={setIsModalSortedVisible} />
+                </Modal>
+              )}
+            </div>
+          </TopPanel>
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className={styles.ExpenseChartContainer}>
+              <div className={styles.TotalAmountHeader}>
+                <h3>Total Expenses: <span>{dataChart.totalAmount} ZŁ</span></h3>
+              </div>
+              <div className={styles.ChartsContainer}>
+                {selectedChart === 'pie' && (
+                  <PieChartComponent dataChart={dataChart} />
+                )}
+                {selectedChart === 'column' && (
+                  <BarChartComponent dataChart={dataChart} name={0} />
+                )}
+                <div className={styles.ChartsItemsContainer}>
+                  {dataChart.categories.map((category) => (
+                    <ChartItem categoryItem={category} />
+                  ))}
                 </div>
-            </TopPanel>
-            {loading ? (
-                <Loader />
-            ) : (
-                <div className={styles.ExpenseChartContainer}>
-                    <div className={styles.TotalAmountHeader}>
-                        <h3>Total Expenses: <span>{dataChart.totalAmount} ZŁ</span></h3>
-                    </div>
-                    <div className={styles.ChartsContainer}>
-                        <PieChartComponent 
-                            dataChart={dataChart}
-                        />
-                        <div className={styles.ChartsItemsContainer}>
-                            {dataChart.categories.map((category) => (
-                                <ChartItem 
-                                    categoryItem={category}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                )
-            } 
+              </div>
+            </div>
+          )}
         </div>
     );
 };
