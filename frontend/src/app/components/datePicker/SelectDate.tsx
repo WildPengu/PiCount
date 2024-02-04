aeb0c25bbdb21b0e0d30c6a020a947e0ebf4c85e
YarynaHuk <jasmin.huk@gmail.com>
2024-02-04 18:17:09 +0100

    set default value in input for date and category in FilterPanel component

--- frontend/src/app/components/datePicker/SelectDate.tsx
+++ frontend/src/app/components/datePicker/SelectDate.tsx
@@ -1,44 +1,52 @@
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
-import styles from './SelectDate.module.scss';
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import styles from "./SelectDate.module.scss";
 
 export interface SelectDateProps {
-    setDate?: (date: string) => void;
-    label?: 'Select Date' | 'From' | 'To';
+  setDate?: (date: string) => void;
+  label?: "Select Date" | "From" | "To";
+  selectModal?: string;
 }
 
-export const SelectDate = ({ setDate, label = 'Select Date' }: SelectDateProps) => {
+export const SelectDate = ({
+  setDate,
+  label = "Select Date",
+  selectModal,
+}: SelectDateProps) => {
+  const [day, setDay] = useState("");
 
-    const [day, setDay] = useState('');
+  useEffect(() => {
+    if (selectModal === "filter") {
+      setDay("2023-01-01");
+    } else {
+      const formattedDate = dayjs(new Date()).format("YYYY-MM-DD");
+      setDay(formattedDate);
+    }
+  }, []);
 
-    useEffect(() => {
-        const formattedDate = dayjs(new Date()).format('YYYY-MM-DD');
-        setDay(formattedDate);
-    }, []);
+  const handleChange = (newDate: any) => {
+    const formattedDate = dayjs(newDate).format("YYYY-MM-DD");
+    setDay(formattedDate);
+  };
 
-    const handleChange = (newDate: any) => {
-        const formattedDate = dayjs(newDate).format('YYYY-MM-DD');
-        setDay(formattedDate);
-    };
+  useEffect(() => {
+    setDate && setDate(day);
+  }, [day]);
 
-    useEffect(() => {
-        setDate && setDate(day);
-    }, [day]);
-
-    return (
-        <div>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                    className={styles.SelectDate} 
-                    label={label}
-                    minDate={dayjs('2023-01-01')}
-                    maxDate={dayjs(Date())}
-                    onChange={(newDate) => handleChange(newDate)}
-                    value={dayjs(day)}
-                    />
-                </LocalizationProvider>
-        </div>
-    );
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          className={styles.SelectDate}
+          label={label}
+          minDate={dayjs("2023-01-01")}
+          maxDate={dayjs(Date())}
+          onChange={(newDate) => handleChange(newDate)}
+          value={selectModal === "filter" ? dayjs("2023-01-01") : dayjs(day)}
+        />
+      </LocalizationProvider>
+    </div>
+  );
 };
--- frontend/src/app/components/selectCategory/SelectCategory.tsx
+++ frontend/src/app/components/selectCategory/SelectCategory.tsx
@@ -1,73 +1,83 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import styles from './Selectcategory.module.scss';
-import { AppSettingsProvider } from '../../config';
+import { ChangeEvent, useEffect, useState } from "react";
+import styles from "./Selectcategory.module.scss";
+import { AppSettingsProvider } from "../../config";
 
 export interface Category {
-    _id: string;
-    name: string;
+  _id: string;
+  name: string;
 }
 
 export interface SelectCategoryProps {
   setCategory?: (category: string) => void;
+  selectModal: string;
 }
 
-export const SelectCategory = ({ setCategory }: SelectCategoryProps) => {
+export const SelectCategory = ({
+  selectModal,
+  setCategory,
+}: SelectCategoryProps) => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [activeOption, setActiveOption] = useState<string>("");
+  const { appSettings } = AppSettingsProvider();
 
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [activeOption, setActiveOption] = useState<string>('');
-    const { appSettings } = AppSettingsProvider();
-    
-    useEffect(() => {
-        async function fetchCategories() {
-          try {
-            const response = await fetch(`${appSettings.apiHost}:${appSettings.apiPort}/expensesCategories`);
-    
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setCategories(data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await fetch(
+          `${appSettings.apiHost}:${appSettings.apiPort}/expensesCategories`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-        fetchCategories();
-      }, []);
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchCategories();
+  }, []);
 
-    const handleChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
-        const selectedCategoryName = e.target.value;
-        setActiveOption(selectedCategoryName);
-    };
+  const handleChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategoryName = e.target.value;
+    setActiveOption(selectedCategoryName);
+  };
 
-    useEffect(() => {
-      setActiveOption('Shopping');
-    }, []);
+  useEffect(() => {
+    if (selectModal === "filter") {
+      setActiveOption("All");
+    } else {
+      setActiveOption("Shopping");
+    }
+  }, []);
 
-    useEffect(() => {
-      setCategory && setCategory(activeOption);
+  useEffect(() => {
+    setCategory && setCategory(activeOption);
   }, [activeOption]);
-    
-    return (
-        <div className={styles.SelectCategory}>
-            <label
-                htmlFor='typeOfExpense'
-                className={styles.CategoryLabel}
-            >Select category</label>
-            <select
-                id='typeOfExpense'
-                className={styles.CategoryOfExpense}
-                onChange={(e) => handleChangeCategory(e)}
-                value={activeOption}
-            >
-                {categories.map((category) => (
-                <option 
-                  key={category._id} 
-                  value={category.name}
-                >
-                  {category.name}
-                </option>
-                 ))}
-            </select>
-        </div>
-    );
+
+  return (
+    <div className={styles.SelectCategory}>
+      <label htmlFor='typeOfExpense' className={styles.CategoryLabel}>
+        Select category
+      </label>
+      <select
+        id='typeOfExpense'
+        className={styles.CategoryOfExpense}
+        onChange={(e) => handleChangeCategory(e)}
+        value={activeOption}
+      >
+        {selectModal === "filter" ? (
+          <option key={"All"} value={"All"}>
+            All
+          </option>
+        ) : null}
+        {categories.map((category) => (
+          <option key={category._id} value={category.name}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
