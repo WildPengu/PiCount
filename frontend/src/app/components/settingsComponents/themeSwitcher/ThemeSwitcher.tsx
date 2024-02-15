2c42e7a9d666af875ef44baf25f9adaa104ebd66
YarynaHuk <jasmin.huk@gmail.com>
2024-02-15 13:19:03 +0100

    added theme toggle

--- frontend/src/app/components/settingsComponents/themeSwitcher/ThemeSwitcher.tsx
+++ frontend/src/app/components/settingsComponents/themeSwitcher/ThemeSwitcher.tsx
@@ -6,13 +6,14 @@ export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div className={styles.ThemeSwitcherContainer}>
+    <div className={`${styles.ThemeSwitcherContainer} ${theme}Container`}>
       <h2>Theme Switcher</h2>
       <label className={styles.SwitchLabel}>
         <input
           type='checkbox'
           className={styles.SliderInput}
-          onChange={() => toggleTheme()}
+          checked={theme === "LightTheme"}
+          onChange={toggleTheme}
         />
         <span className={styles.Slider + " " + styles.Round}></span>
       </label>
