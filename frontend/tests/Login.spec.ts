2054cbc3de41128faaf77734919261beceec859f
YarynaHuk <jasmin.huk@gmail.com>
2024-03-17 14:15:15 +0100

    WIP

--- frontend/tests/Login.spec.ts
+++ frontend/tests/Login.spec.ts
@@ -1,32 +1,35 @@
 import { test, expect } from "@playwright/test";
 
-test("login form test with correct data", async ({ page }) => {
-  await page.goto("http://localhost:5173/login");
+test("login form test with correct data and logout", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Login" }).click();
 
-  await page.fill('input[name="username"]', "yarna");
-  await page.fill('input[name="password"]', "!23456");
-
-  await page.click('button[type="submit"]');
+  await page.getByPlaceholder("User name").click();
+  await page.getByPlaceholder("User name").fill("yarna");
+  await page.getByPlaceholder("User password").click();
+  await page.getByPlaceholder("User password").fill("!23456");
+  await page.getByRole("button", { name: "Login" }).click();
 
   await page.waitForTimeout(3000);
 
   const currentUrl = page.url();
   expect(currentUrl).toEqual("http://localhost:5173/expenseList");
+
+  await page.getByRole("heading", { name: "yarna" }).click();
+  await page.getByRole("link", { name: "Logout" }).click();
+  await page.getByRole("heading", { name: "PiCount", exact: true }).click();
 });
 
 test("login form test with incorrect data", async ({ page }) => {
   await page.goto("http://localhost:5173/login");
 
-  await page.fill('input[name="username"]', "bulbazavr");
-  await page.fill('input[name="password"]', "!23456");
+  await page.getByPlaceholder("User name").click();
+  await page.getByPlaceholder("User name").fill("agasia");
+  await page.getByPlaceholder("User password").click();
+  await page.getByPlaceholder("User password").fill("12qwerty");
+  await page.getByRole("button", { name: "Login" }).click();
 
-  await page.click('button[type="submit"]');
-
-  const errorMessageSelector = "form h2";
-  await page.waitForSelector(errorMessageSelector);
-
-  const errorMessage = await page.textContent(errorMessageSelector);
-  expect(errorMessage).toContain("Incorrect username or password.");
+  await page.getByRole("heading", { name: "Incorrect username or" }).click();
 
   await page.waitForTimeout(3000);
 
---
+++ frontend/tests/SignUp.spec.ts
@@ -0,0 +1,64 @@
+import { test } from "@playwright/test";
+
+test("SignUp with empty or empty data should display error messages", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173/signUp");
+
+  await page.getByRole("button", { name: "SignUp" }).click();
+
+  await page.getByText("User name should have at").click();
+  await page.getByText("Please selecte Avatar*").click();
+  await page.getByText("There is no valid email*").click();
+  await page.getByText("Password should have at least").click();
+
+  //error message if confirm password is not the same
+
+  await page.getByPlaceholder("Password", { exact: true }).click();
+  await page.getByPlaceholder("Password", { exact: true }).fill("!23456");
+  await page.getByPlaceholder("Confirm password").click();
+  await page.getByPlaceholder("Confirm password").fill("123456");
+  await page.getByRole("button", { name: "SignUp" }).click();
+  await page.getByText("Password should be the same*").click();
+});
+
+test("Successful SignUp", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Create account" }).click();
+
+  //enter data for new user
+
+  await page
+    .getByRole("heading", { name: "Fill out the fields to create" })
+    .click();
+  await page.getByPlaceholder("User name").click();
+  await page.getByPlaceholder("User name").fill("kasia3.0");
+  await page.getByPlaceholder("User age").click();
+  await page.getByPlaceholder("User age").fill("24");
+  await page.getByRole("img", { name: "Avatar 8" }).click();
+  await page.getByPlaceholder("User email").click();
+  await page.getByPlaceholder("User email").fill("kasia3.0@gmail.com");
+  await page.getByPlaceholder("Password", { exact: true }).click();
+  await page.getByPlaceholder("Password", { exact: true }).fill("!23456");
+  await page.getByPlaceholder("Confirm password").click();
+  await page.getByPlaceholder("Confirm password").fill("!23456");
+
+  //create new user
+
+  await page.getByRole("button", { name: "SignUp" }).click();
+
+  await page.waitForTimeout(3000);
+
+  await page.getByRole("heading", { name: "Welcome, new PiCounter!" }).click();
+  await page.getByRole("link", { name: "Go to login" }).click();
+
+  //login
+
+  await page.getByPlaceholder("User name").click();
+  await page.getByPlaceholder("User name").fill("kasia3.0");
+  await page.getByPlaceholder("User password").click();
+  await page.getByPlaceholder("User password").fill("!23456");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.getByRole("heading", { name: "kasia3.0" }).click();
+});
