529aaa211af2db6c2f270e1eea3cd3117b2ad297
WildPengu <7241679@wp.pl>
2024-03-20 20:52:56 +0100

    Added delete and patch crypto

--- frontend/src/app/components/investment/crypto/myAssets/MyAssets.tsx
+++ frontend/src/app/components/investment/crypto/myAssets/MyAssets.tsx
@@ -6,6 +6,7 @@ import styles from './MyAssets.module.scss';
 import { AssetRow } from './assetRow/AssetRow';
 
 export interface Crypto {
+  cryptoId: string;
   name: string;
   symbol: string;
   amount: number;
@@ -36,6 +37,29 @@ export const MyAssets = () => {
     fetchData();
   }, []);
 
+  async function deleteCryptoAsset(cryptoId: string, symbol: string) {
+    try {
+      const response = await fetch(`${appSettings.apiHost}:${appSettings.apiPort}/assets/crypto/${appSettings.user_id}/${cryptoId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete crypto asset');
+      }
+
+      const updateCrypto = { ...crypto };
+      delete updateCrypto[symbol];
+      setCrypto(updateCrypto);
+
+    } catch (error: any) {
+      console.error('Error deleting crypto asset:', error.message);
+    }
+
+  }
+
   const addCrypto = (symbol: string, amount: number) => {
     fetch(`${appSettings.apiHost}:${appSettings.apiPort}/assets/crypto/${appSettings.user_id}`, {
       method: 'PATCH',
@@ -52,9 +76,37 @@ export const MyAssets = () => {
       });
   };
 
+  const updateCryptoAmount = (amount: number, cryptoId: string, symbol: string) => {
+    const url = `${appSettings.apiHost}:${appSettings.apiPort}/assets/crypto/${appSettings.user_id}/${cryptoId}?amount=${amount}`;
+
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        amount
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(updatedCrypto => {
+        const allCrypto = { ...crypto };
+        allCrypto[symbol].amount = updatedCrypto.amount;
+        setCrypto(allCrypto);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+
   let totalAssetsPrice = 0;
 
-  const cryptoRows = _.map(crypto as any, (row: Crypto, index: number) => {
+  const cryptoRows = _.map(crypto as Record<string, any>, (row: Crypto, index: number) => {
     if (row.quote && row.quote.USD && row.quote.USD.price) {
       const cryptoValue = parseFloat((row.amount * row.quote.USD.price).toFixed(2));
       totalAssetsPrice += cryptoValue;
@@ -62,11 +114,9 @@ export const MyAssets = () => {
       return (
         <AssetRow
           key={index}
-          name={row.name}
-          symbol={row.symbol}
-          amount={row.amount}
-          logo={row.logo}
-          quote={row.quote}
+          row={row}
+          deleteCryptoAsset={deleteCryptoAsset}
+          updateCryptoAmount={updateCryptoAmount}
         />
       );
     }
@@ -80,13 +130,16 @@ export const MyAssets = () => {
         <div>Amount</div>
         <div>Coin Price</div>
         <div>Today's PnL</div>
+        <div>24h %</div>
+        <div>7d %</div>
+        <div>30d %</div>
       </div>
       <div className={styles.assetsList}>
         {crypto ? cryptoRows : <Loader />}
       </div>
       <div>
         <div>
-          <span>Amount</span>
+          <span>Amount:</span>
           <input
             name='amount'
             type='number'
@@ -97,7 +150,7 @@ export const MyAssets = () => {
           />
         </div>
         <div>
-          <span>Symbol</span>
+          <span>Symbol:</span>
           <input
             name='symbol'
             type='string'
--- frontend/src/app/components/investment/crypto/myAssets/assetRow/AssetRow.tsx
+++ frontend/src/app/components/investment/crypto/myAssets/assetRow/AssetRow.tsx
@@ -1,16 +1,28 @@
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Crypto } from '../MyAssets';
 import styles from './assetRow.module.scss';
 
-export const AssetRow: React.FC<Crypto> = ({ name, symbol, amount, logo, quote }) => {
-  const { price, percent_change_24h } = quote.USD;
+interface AssetRowProps {
+  key: number,
+  row: Crypto,
+  deleteCryptoAsset: (cryptoId: string, symbol: string) => Promise<void>;
+  updateCryptoAmount: (amount: number, cryptoId: string, symbol: string) => Promise<void>;
+}
+
+export const AssetRow: React.FC<AssetRowProps> = ({ key, row, deleteCryptoAsset, updateCryptoAmount }) => {
+  const { amount, logo, name, symbol, cryptoId, quote } = row;
+  const { price, percent_change_24h, percent_change_7d, percent_change_30d } = quote.USD;
 
   const currentValue = amount * price;
   const price_before_24h = price / (1 + (percent_change_24h / 100));
 
   const profitInDollars = currentValue - (amount * price_before_24h);
 
+
+
   return (
-    <div className={styles.assetRow}>
+    <div className={styles.assetRow} key={key}>
       <div className={styles.firstColumn}>
         <img src={logo} className={styles.image} />
         <div className={styles.column}>
@@ -20,15 +32,25 @@ export const AssetRow: React.FC<Crypto> = ({ name, symbol, amount, logo, quote }
       </div>
 
       <div className={styles.column}>
-        <div className={styles.upperItem}>{amount.toFixed(2)}</div>
+        <div contentEditable="true" onBlur={(e) => updateCryptoAmount(parseFloat(e.target.textContent), cryptoId, symbol)} className={styles.upperItem}>{amount.toFixed(2)}</div>
         <div className={styles.lowerItem}>${(amount * price).toFixed(2)}</div>
       </div>
 
       <div className={styles.price}>${price.toFixed(2)}</div>
       <div
-        className={styles.todaysPnL}
         style={{ color: profitInDollars > 0 ? '#0fac6f' : '#EF454A' }}
-      >${profitInDollars.toFixed(2)}</div>
+      >
+        ${profitInDollars.toFixed(2)}
+      </div>
+      <div style={{ color: percent_change_24h > 0 ? '#0fac6f' : '#EF454A' }}>{percent_change_24h.toFixed(2)}%</div>
+      <div style={{ color: percent_change_7d > 0 ? '#0fac6f' : '#EF454A' }}>{percent_change_7d.toFixed(2)}%</div>
+      <div style={{ color: percent_change_30d > 0 ? '#0fac6f' : '#EF454A' }}>{percent_change_30d.toFixed(2)}%</div>
+      <div
+        className={styles.trashIcon}
+        onClick={() => deleteCryptoAsset(cryptoId, symbol)}
+      >
+        <FontAwesomeIcon icon={faTrashCan} />
+      </div>
     </div>
   )
 }
