1bd69cd35a7685be7c20b939c368390752b14952
YarynaHuk <jasmin.huk@gmail.com>
2024-09-15 15:57:23 +0200

    added missing tests for expenseList

---
+++ frontend/tests/ExpenseList.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+//all tests in english app version
+
+test.describe("Expense list tests", () => {
+  const urlExpense = "http://localhost:5173/expenseList";
+
+  test("successful adding new expense", async ({ page }) => {
+    await page.goto(urlExpense);
+
+    await page.getByTestId("add-expense-btn").click();
+
+    await expect(page.getByTestId('add-expense-h2')).toHaveText("Add New Expense");
+
+    await page.getByLabel("Select category").selectOption("Transport");
+    await page.getByPlaceholder("Write sth...").fill("washing and clean car");
+    await page.getByPlaceholder("Amount").fill("16");
+    await page.getByRole("button", { name: "Add expense" }).click();
+    await page.getByRole("heading", { name: "TODAY" }).click();
+    await page.getByText("Transport").first().click();
+    await page.getByText("fuel").click();
+    await page.getByText("- 200 ZŁ").first().click();
+  });
+});
