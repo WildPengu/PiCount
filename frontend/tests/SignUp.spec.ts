40570e6b0f4fc330cc420e6811823b24f780b86c
WildPengu <7241679@wp.pl>
2024-10-03 19:08:01 +0200

    SignUp test now add user every time

--- frontend/tests/SignUp.spec.ts
+++ frontend/tests/SignUp.spec.ts
@@ -1,85 +1,84 @@
 import { expect, test } from "@playwright/test";
+import { URL_HOME } from './utils/constants';
 
-test.describe("Sign Up tests", () => {
-  const urlHome = "http://localhost:5173/";
+test.describe('Sign Up tests', () => {
+  const userPassword = '!23456';
+  const wrongPassword = '123456';
+  const newUserAge = '24';
+  const newUsername = `user_${Date.now()}`;
+  const newUserEmail = `user_${Date.now()}@gmail.com`;
 
-  const newUsername = "kasia";
-  const userPassword = "!23456";
-  const wrongPassword = "123456";
-  const newUserAge = "24";
-  const newUserEmail = "kasia@gmail.com";
-
-  test("SignUp with empty or incorrect data should display error messages", async ({
+  test('SignUp with empty or incorrect data should display error messages', async ({
     page,
   }) => {
-    const usernameError = "User name should have at least 4 characters*";
-    const avatarError = "Please selecte Avatar*";
-    const emailError = "There is no valid email*";
-    const passwordError = "Password should have at least 6 characters*";
-    const confirmPasswordError = "Password should be the same*";
+    const usernameError = 'User name should have at least 4 characters*';
+    const avatarError = 'Please selecte Avatar*';
+    const emailError = 'There is no valid email*';
+    const passwordError = 'Password should have at least 6 characters*';
+    const confirmPasswordError = 'Password should be the same*';
 
-    await page.goto(`${urlHome}signUp`);
-    await page.getByRole("button", { name: "SignUp" }).click();
+    await page.goto(`${URL_HOME}signUp`);
+    await page.getByRole('button', { name: 'SignUp' }).click();
 
     //if website in english
 
-    await expect(page.getByTestId("username-error")).toHaveText(usernameError);
-    await expect(page.getByTestId("avatar-error")).toHaveText(avatarError);
-    await expect(page.getByTestId("email-error")).toHaveText(emailError);
-    await expect(page.getByTestId("password-error")).toHaveText(passwordError);
+    await expect(page.getByTestId('username-error')).toHaveText(usernameError);
+    await expect(page.getByTestId('avatar-error')).toHaveText(avatarError);
+    await expect(page.getByTestId('email-error')).toHaveText(emailError);
+    await expect(page.getByTestId('password-error')).toHaveText(passwordError);
 
     //error message if confirm password is not the same
 
-    await page.getByPlaceholder("Password", { exact: true }).fill(userPassword);
-    await page.getByPlaceholder("Confirm password").fill(wrongPassword);
-    await page.getByRole("button", { name: "SignUp" }).click();
-    await expect(page.getByTestId("confirm-password-error")).toHaveText(
+    await page.getByPlaceholder('Password', { exact: true }).fill(userPassword);
+    await page.getByPlaceholder('Confirm password').fill(wrongPassword);
+    await page.getByRole('button', { name: 'SignUp' }).click();
+    await expect(page.getByTestId('confirm-password-error')).toHaveText(
       confirmPasswordError
     );
   });
 
-  test("Successful SignUp", async ({ page }) => {
-    const headerMessage = "Fill out the fields to create a new user:";
-    const welcomeMessage = "Welcome, new PiCounter!";
+  test('Successful SignUp', async ({ page }) => {
+    const headerMessage = 'Fill out the fields to create a new user:';
+    const welcomeMessage = 'Welcome, new PiCounter!';
 
-    await page.goto(urlHome);
-    await page.getByRole("link", { name: "Create account" }).click();
+    await page.goto(URL_HOME);
+    await page.getByRole('link', { name: 'Create account' }).click();
 
     //enter data for new user
 
-    await expect(page.getByTestId("welcome-message")).toHaveText(headerMessage);
+    await expect(page.getByTestId('welcome-message')).toHaveText(headerMessage);
 
-    await page.getByPlaceholder("User name").fill(newUsername);
-    await page.getByPlaceholder("User age").fill(newUserAge);
-    await page.getByRole("img", { name: "Avatar 8" }).click();
-    await page.getByPlaceholder("User email").fill(newUserEmail);
+    await page.getByPlaceholder('User name').fill(newUsername);
+    await page.getByPlaceholder('User age').fill(newUserAge);
+    await page.getByRole('img', { name: 'Avatar 8' }).click();
+    await page.getByPlaceholder('User email').fill(newUserEmail);
 
-    await page.getByPlaceholder("Password", { exact: true }).fill(userPassword);
-    await page.getByPlaceholder("Confirm password").fill(userPassword);
+    await page.getByPlaceholder('Password', { exact: true }).fill(userPassword);
+    await page.getByPlaceholder('Confirm password').fill(userPassword);
 
     //create new user
 
-    await page.getByRole("button", { name: "SignUp" }).click();
+    await page.getByRole('button', { name: 'SignUp' }).click();
 
     await page.waitForTimeout(3000);
 
-    await expect(page.getByTestId("welcome-message")).toHaveText(
+    await expect(page.getByTestId('welcome-message')).toHaveText(
       welcomeMessage
     );
 
-    await page.getByTestId("link-to-login").click();
+    await page.getByTestId('link-to-login').click();
 
     await page.waitForTimeout(3000);
 
     const currentUrl = page.url();
-    expect(currentUrl).toEqual(`${urlHome}login`);
+    expect(currentUrl).toEqual(`${URL_HOME}login`);
 
     //login
 
-    await page.getByPlaceholder("User name").fill(newUsername);
-    await page.getByPlaceholder("User password").fill(userPassword);
-    await page.getByRole("button", { name: "Login" }).click();
+    await page.getByPlaceholder('User name').fill(newUsername);
+    await page.getByPlaceholder('User password').fill(userPassword);
+    await page.getByRole('button', { name: 'Login' }).click();
 
-    await expect(page.getByTestId("username")).toHaveText(newUsername);
+    await expect(page.getByTestId('username')).toHaveText(newUsername);
   });
 });
