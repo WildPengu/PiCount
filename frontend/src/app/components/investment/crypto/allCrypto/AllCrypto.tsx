48f8802f31c57b65512600f7df3eb92b9128601c
YarynaHuk <jasmin.huk@gmail.com>
2025-02-02 19:03:51 +0100

    add sort arrow

--- frontend/src/app/components/investment/crypto/allCrypto/AllCrypto.tsx
+++ frontend/src/app/components/investment/crypto/allCrypto/AllCrypto.tsx
@@ -1,22 +1,23 @@
-
 import { useEffect, useState } from 'react';
 import { AppSettingsProvider } from '../../../../config';
 import { Loader } from '../../../loader/Loader';
 import styles from './AllCrypto.module.scss';
 import { CryptoRow } from './cryptoRow/CryptoRow';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 export const AllCrypto = () => {
   const { appSettings } = AppSettingsProvider();
   const [allCrypto, setAllCrypto] = useState<any>(null);
-  const [sortBy, setSortBy] = useState<string>("market_cap");
-  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+  const [sortBy, setSortBy] = useState<string>('market_cap');
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc');
 
   const handleSort = (field: string) => {
     if (sortBy === field) {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
     } else {
       setSortBy(field);
-      setSortOrder("asc");
+      setSortOrder('asc');
     }
   };
 
@@ -41,13 +42,18 @@ export const AllCrypto = () => {
     fetchData();
   }, [sortOrder, sortBy]);
 
+  const getArrow = (order: string, field: string): any => {
+    if (field !== sortBy) return;
+    if (order === 'desc') {
+      return <FontAwesomeIcon icon={faCaretDown} />;
+    }
+    return <FontAwesomeIcon icon={faCaretUp} />;
+  };
+
   const cryptoRows = allCrypto?.map((row: Cryptocurrency, index: number) => {
     return (
       <div key={`${row.id}-${index}`}>
-        <CryptoRow
-          index={index}
-          row={row}
-        />
+        <CryptoRow index={index} row={row} />
       </div>
     );
   });
@@ -56,19 +62,61 @@ export const AllCrypto = () => {
     <div className={styles.assetsContainer}>
       <div className={styles.sortHeader}>
         <div>#</div>
-        <div onClick={() => handleSort("name")}>Name</div>
-        <div className={styles.alignRight} onClick={() => handleSort("price")}>Price</div>
-        <div className={styles.alignRight} onClick={() => handleSort("percent_change_1h")}>1h %</div>
-        <div className={styles.alignRight} onClick={() => handleSort("percent_change_24h")}>24h %</div>
-        <div className={styles.alignRight} onClick={() => handleSort("percent_change_7d")}>7d %</div>
-        <div className={styles.alignRight} onClick={() => handleSort("percent_change_30d")}>30d %</div>
-        <div className={styles.alignRight} onClick={() => handleSort("circulating_supply")}>Circulating Supply</div>
-        <div className={styles.alignRight} onClick={() => handleSort("marketCap")}>Market Cap</div>
+        <div onClick={() => handleSort('name')}>
+          {getArrow(sortOrder, 'name')}
+          Name
+        </div>
+        <div className={styles.alignRight} onClick={() => handleSort('price')}>
+          {getArrow(sortOrder, 'price')}
+          Price
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('percent_change_1h')}
+        >
+          {getArrow(sortOrder, 'percent_change_1h')}
+          1h %
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('percent_change_24h')}
+        >
+          {getArrow(sortOrder, 'percent_change_24h')}
+          24h %
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('percent_change_7d')}
+        >
+          {getArrow(sortOrder, 'percent_change_7d')}
+          7d %
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('percent_change_30d')}
+        >
+          {getArrow(sortOrder, 'percent_change_30d')}
+          30d %
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('circulating_supply')}
+        >
+          {getArrow(sortOrder, 'circulating_supply')}
+          Circulating Supply
+        </div>
+        <div
+          className={styles.alignRight}
+          onClick={() => handleSort('marketCap')}
+        >
+          {getArrow(sortOrder, 'marketCap')}
+          Market Cap
+        </div>
         <div className={styles.alignRight}>Last 7 days</div>
       </div>
       <div className={styles.assetsList}>
         {allCrypto ? cryptoRows : <Loader />}
       </div>
     </div>
-  )
-}
+  );
+};
